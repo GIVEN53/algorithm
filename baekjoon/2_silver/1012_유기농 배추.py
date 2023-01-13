f38a461c20d5915c312c956db0acf4755dc22010
NAM GIBEOM <rlqja0523@naver.com>
2023-01-13 23:33:05 +0900

    feat: baekjoon 1012

---
+++ baekjoon/2_silver/1012_유기농 배추.py
@@ -0,0 +1,46 @@
+from collections import deque
+from sys import stdin
+
+
+def bfs(x, y):
+    global visited
+
+    q = deque([(x, y)])
+    visited[x][y] = True
+
+    while q:
+        x, y = q.popleft()
+
+        if (x, y) not in graph:
+            continue
+        
+        for i in range(4):
+            a = x + row[i]
+            b = y + col[i]
+        
+            if a > -1 and a < M and b > -1 and b < N and not visited[a][b]:
+                visited[a][b] = True
+                q.append((a, b))
+
+
+row = [0, 0, -1, 1]
+col = [-1 , 1, 0, 0]
+T = int(stdin.readline())
+
+for _ in range(T):
+    M, N, K = map(int, stdin.readline().split())
+    graph = set()
+    visited = [[False] * N for _ in range(M)]
+
+    for _ in range(K):
+        X, Y = map(int, stdin.readline().split())
+        graph.add((X, Y))
+
+    cnt = 0
+    for now in graph:
+        x = now[0]
+        y = now[1]
+        if not visited[x][y]:
+            bfs(x, y)
+            cnt += 1
+    print(cnt)
