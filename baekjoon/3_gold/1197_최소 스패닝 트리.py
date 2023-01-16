ef397dc2af1a8eafbd80db260213d2166d058662
NAM GIBEOM <rlqja0523@naver.com>
2023-01-16 13:37:43 +0900

    feat: baekjoon 1197

---
+++ baekjoon/3_gold/1197_최소 스패닝 트리.py
@@ -0,0 +1,36 @@
+from heapq import heappop, heappush
+from sys import stdin
+
+
+def bfs(start):
+    visited[start] = True
+    q = []
+    for next in graph[start]:
+        heappush(q, next)
+    
+    min_weight = 0
+    while q:
+        weight, now = heappop(q)
+
+        if visited[now]:
+            continue
+        
+        min_weight += weight
+        visited[now] = True
+        for next in graph[now]:
+            heappush(q, next)
+    
+    return min_weight
+
+
+V, E = map(int, stdin.readline().split())
+
+graph = [[] for _ in range(V + 1)]
+visited = [False] * (V + 1)
+
+for _ in range(E):
+    a, b, c = map(int, stdin.readline().split())
+    graph[a].append((c, b))
+    graph[b].append((c, a))
+
+print(bfs(1))
