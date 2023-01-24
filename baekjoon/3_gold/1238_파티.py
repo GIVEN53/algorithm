d67473eba62e66539da29c2f49b35ccf67bea295
NAM GIBEOM <rlqja0523@naver.com>
2023-01-24 18:53:08 +0900

    feat: baekjoon 1238

---
+++ baekjoon/3_gold/1238_파티.py
@@ -0,0 +1,71 @@
+from heapq import heappop, heappush
+from sys import stdin
+
+
+# 1
+def dijkstra(start):
+    distance[start][start] = 0
+    q = []
+    heappush(q, (0, start))
+    while q:
+        dist, node = heappop(q)
+
+        for next, next_dist in graph[node].items():
+            cost = dist + next_dist
+            if cost < distance[start][next]:
+                distance[start][next] = cost
+                heappush(q, (cost, next))
+
+
+N, M, X = map(int, stdin.readline().split())
+graph = [{} for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b, t = map(int, stdin.readline().split())
+    graph[a][b] = t
+
+distance = [[1e9] * (N + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    dijkstra(i)
+
+max_distance = 0
+for i in range(1, N + 1):
+    max_distance = max(max_distance, distance[i][X] + distance[X][i])
+print(max_distance)
+
+
+# 2
+def dijkstra(start, graph):
+    distance = [1e9] * (N + 1)
+    distance[start] = 0
+    q = []
+    heappush(q, (0, start))
+    while q:
+        dist, node = heappop(q)
+
+        for next, next_dist in graph[node].items():
+            cost = dist + next_dist
+            if cost < distance[next]:
+                distance[next] = cost
+                heappush(q, (cost, next))
+
+    return distance
+
+
+N, M, X = map(int, stdin.readline().split())
+graph = [{} for _ in range(N + 1)]
+reverse_graph = [{} for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b, t = map(int, stdin.readline().split())
+    graph[a][b] = t
+    reverse_graph[b][a] = t
+
+distance = dijkstra(X, graph)
+reverse_distance = dijkstra(X, reverse_graph)
+
+max_distance = 0
+for i in range(1, N + 1):
+    max_distance = max(max_distance, distance[i] + reverse_distance[i])
+print(max_distance)
