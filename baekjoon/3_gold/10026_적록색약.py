b4c2d122a87d27ec4a63dcebe24d431056808072
NAM GIBEOM <rlqja0523@naver.com>
2023-02-25 22:11:55 +0900

    feat: bfs 추가

--- baekjoon/3_gold/10026_적록색약.py
+++ baekjoon/3_gold/10026_적록색약.py
@@ -1,6 +1,8 @@
 from sys import stdin, setrecursionlimit
+from collections import deque
 
 
+# 1
 def dfs(x, y, color_weakness):
     global rg_visited, rgb_visited
 
@@ -52,3 +54,42 @@ for i in range(n):
             dfs(i, j, False)
 
 print(rgb_count, rg_count)
+
+
+# 2
+def bfs(diff_color):
+    visited = [[False] * n for _ in range(n)]
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                continue
+
+            count += 1
+            q = deque([(i, j)])
+            while q:
+                x, y = q.popleft()
+                for k in range(4):
+                    nx = x + direction[k]
+                    ny = y + direction[3 - k]
+
+                    if is_out_of_range(nx, ny) or visited[nx][ny] or board[nx][ny] in diff_color[board[i][j]]:
+                        continue
+
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+    return count
+
+
+def is_out_of_range(x, y):
+    return x < 0 or x > n - 1 or y < 0 or y > n - 1
+
+
+n = int(stdin.readline())
+board = [list(stdin.readline().rstrip()) for _ in range(n)]
+
+diff_color_rgb = {'R': {'G', 'B'}, 'G': {'R', 'B'}, 'B': {'R', 'G'}}
+diff_color_rg = {'R': {'B'}, 'G': {'B'}, 'B': {'R', 'G'}}
+direction = [0, 0, 1, -1]
+
+print(bfs(diff_color_rgb), bfs(diff_color_rg))
