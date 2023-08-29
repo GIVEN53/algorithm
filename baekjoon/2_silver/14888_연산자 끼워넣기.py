47dc496d2ac950e1377d5649fa4ca87b96d8e583
NAM GIBEOM <rlqja0523@naver.com>
2023-08-29 20:13:05 +0900

    feat: 알고리즘 스터디 옮기기

--- baekjoon/2_silver/14888_연산자 끼워넣기.py
+++ baekjoon/2_silver/14888_연산자 끼워넣기.py
@@ -7,22 +7,22 @@ def dfs(idx, num):
 
     if add > 0:
         add -= 1
-        dfs(idx+1, num + numbers[idx])
+        dfs(idx + 1, num + numbers[idx])
         add += 1
 
     if sub > 0:
         sub -= 1
-        dfs(idx+1, num - numbers[idx])
+        dfs(idx + 1, num - numbers[idx])
         sub += 1
 
     if mul > 0:
         mul -= 1
-        dfs(idx+1, num * numbers[idx])
+        dfs(idx + 1, num * numbers[idx])
         mul += 1
 
     if div > 0:
         div -= 1
-        dfs(idx+1, int(num / numbers[idx]))
+        dfs(idx + 1, int(num / numbers[idx]))
         div += 1
 
 
@@ -34,3 +34,46 @@ result = []
 dfs(1, numbers[0])
 print(max(result))
 print(min(result))
+
+
+# 2
+from sys import stdin
+
+
+def dfs(res, idx):
+    global max_res, min_res
+
+    if idx == n:
+        max_res = max(max_res, res)
+        min_res = min(min_res, res)
+        return
+
+    for i in range(4):
+        if operators[i] > 0:
+            operators[i] -= 1
+            if i == 0:
+                dfs(res + nums[idx], idx + 1)
+            elif i == 1:
+                dfs(res - nums[idx], idx + 1)
+            elif i == 2:
+                dfs(res * nums[idx], idx + 1)
+            else:
+                dfs(int(res / nums[idx]), idx + 1)
+            operators[i] += 1
+
+
+n = int(stdin.readline())
+nums = list(map(int, stdin.readline().split()))
+operators = list(map(int, stdin.readline().split()))
+
+max_res = int(-1e9)
+min_res = int(1e9)
+dfs(nums[0], 1)
+
+print(max_res)
+print(min_res)
+
+##########################
+#    memory: 31256KB     #
+#    time:   76ms        #
+##########################
