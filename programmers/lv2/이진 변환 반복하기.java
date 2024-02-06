12de6ffa3c9aae73b323d2c28e828a0b17de30fa
Nam Gi Beom <rlqja0523@naver.com>
2024-02-06 11:40:05 +0900

    feat: programmers 이진 변환 반복하기

---
+++ programmers/lv2/이진 변환 반복하기.java
@@ -0,0 +1,15 @@
+package programmers.lv2;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while (!s.equals("1")) {
+            answer[0]++;
+            answer[1] += s.length();
+            s = s.replace("0", "");
+            answer[1] -= s.length();
+            s = Integer.toBinaryString(s.length());
+        }
+        return answer;
+    }
+}
